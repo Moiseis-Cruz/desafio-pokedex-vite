6775ad76d3607b8f0f4d08da176e635fb85cf01a
Moiseis Cruz <moizzito@gmail.com>
2023-12-30 19:25:02 -0300

    I searched for the items in the array. It remains to catch each pokemon..

--- src/components/main/index.jsx
+++ src/components/main/index.jsx
@@ -1,13 +1,49 @@
+import { useEffect, useState } from "react";
+
 async function getPokemons(){
     const response = await fetch("https://pokeapi.co/api/v2/pokemon/?offset=20&limit=20")
     const pokemons = await response.json()
-    return pokemons
+    return pokemons.results
 }
 
 console.log(await getPokemons());
 
-const Main = () => {
-    <main>
-        <h1>Teste</h1>
-    </main>
+export const Main = () => {
+
+    const [ pokemon, setPokemon ] = useState({
+        pokemonList: []
+    })
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const pokeId = await getPokemons()
+
+            setPokemon({
+                pokemonList: pokeId
+            })
+        }
+        fetchData()
+    },[])
+
+    console.log(pokemon);
+
+    return(
+        <main>
+            <ul>
+                {
+                    pokemon.pokemonList.map((item, index) => {
+                        return(
+                            <li key={index}>
+                                <div>
+                                    <img src={item.url} />
+
+                                    <h2>{item.name}</h2>
+                                </div>
+                            </li>
+                        )
+                    })
+                }
+            </ul>
+        </main>
+    )
 }
